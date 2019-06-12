017bdad952c81b106ffd21c75111febfd4aef03a
App Academy Student <student@appacademy.io>
2019-06-12 16:39:12 -0700

    Complete render_content

--- rails_lite/lib/controller_base.rb
+++ rails_lite/lib/controller_base.rb
@@ -11,11 +11,12 @@ class ControllerBase
   def initialize(req, res)
     @req = req
     @res = res
-
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
@@ -27,8 +28,13 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    res['Content-Type'] = content_type
-    res.write(content)
+    if already_built_response?
+      raise "already rendered"
+    else 
+      res['Content-Type'] = content_type
+      res.write(content)
+      @already_built_response = true
+    end
   end
 
   # use ERB and binding to evaluate templates
